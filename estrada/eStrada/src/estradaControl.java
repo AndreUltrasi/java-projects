b61a92d070e0807cde0078875ef2786ac8d0ebdb
André Augusto Nunes Vieira <andreultrasi@gmail.com>
2017-08-03 23:31:57 -0300

    Add files via upload

---
+++ estrada/eStrada/src/estradaControl.java
@@ -0,0 +1,10 @@
+public class estradaControl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        estradaView objView = new estradaView();
+    }
+    
+}
---
+++ estrada/eStrada/src/estradaVo.java
@@ -0,0 +1,9 @@
+public class estradaVo {
+    public String tipos[] = {"Motos", "Carros e Utilitários(2 eixos)",
+    "Caminhões Médios(3 eixos)","Caminhões Grandes(4 eixos)", "Carretas(5 eixos)"
+    , "Veículos acima de 5 eixos"};
+    public double valor[] = {2.30, 4.60, 6.90, 9.20, 12.50, 2.30};
+    public double totalArrecadado = 0;
+    public int totalCategoria[] = {0, 0, 0, 0, 0, 0};
+    
+}
