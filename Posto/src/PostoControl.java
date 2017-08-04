40fdd93f2cc735cd7020804308c101a536023d45
André Augusto Nunes Vieira <andreultrasi@gmail.com>
2017-08-03 23:41:34 -0300

    Add files via upload

---
+++ Posto/src/PostoControl.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Vinnicius Brito; André Cruz; Andre Augusto
+ */
+public class PostoControl {
+
+   
+    public static void main(String[] args) {
+         PostoView objTela = new PostoView();
+        //TODO APLICATIVO VEM AQUI
+    }
+    
+}
